8890c42f7df4b8fd9c1bc20e6ec0d492374048ba
zh0per <beeboplay@gmail.com>
2023-05-31 19:08:01 +0600

    last commit. BSF created

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
-        MyGraph myGraph = new MyGraph();
+        MyGraph<Integer> myGraph = new MyGraph<>();
         Vertex<Integer> s1 = new Vertex<>(1);
         Vertex<Integer> s2 = new Vertex<>(2);
         Vertex<Integer> s3 = new Vertex<>(3);
@@ -15,6 +11,6 @@ public class Main {
         myGraph.addEdge(s2, s4, 7d);
         myGraph.addEdge(s2, s5, 8d);
         myGraph.dijkstra(s1);
-
+        myGraph.BFS(s2);
     }
 }
--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class MyGraph<Vertex> {
-    private Map<Vertex, List<Edge<Vertex>>> graph;
+public class MyGraph<V> {
+    private Map<Vertex, List<Edge<V>>> graph;
 
     public MyGraph() {
         graph = new HashMap<>();
@@ -9,6 +9,7 @@ public class MyGraph<Vertex> {
 
     public void addEdge(Vertex source, Vertex destination, double weight){
         Edge edge = new Edge(source, destination, weight);
+        source.addAdjVertex(destination, weight);
         if (!graph.containsKey(source)) graph.put(source, new ArrayList<>());
         graph.get(source).add(edge);
     }
@@ -42,6 +43,26 @@ public class MyGraph<Vertex> {
         }
 
         return distances;
+    }
+
+    public void BFS(Vertex start){
+        Set<Vertex> visited = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        visited.add(start);
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex currentVertex = queue.poll();
+            System.out.print(currentVertex + " ");
+            Set<Vertex<V>> neighbors = currentVertex.getAdjVertices().keySet();
+            for (Vertex<V> neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
     }
 
 }
